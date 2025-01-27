d30e905fa48168eabfc6b79061b6b8ae932508c7
Durga Prasad <pdp49507@gmail.com>
2025-01-27 19:39:50 +0530

    Assignment1 - Data Types

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -3,7 +3,51 @@ package com.assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+         float temparature =25.5f;
+         
+         boolean customerorder=true;
+         
+         String phonenumber="123-456-7890";
+         
+         float accountbalance=1000.25f;
+         
+         String email="john.doe@example.com";
+         
+         double latitude =37.7749;
+         
+         double longitude =-122.4194;
+         
+         boolean maritalstatus=true;
+         
+         String occupation="Software-engineer";
+         
+         String color="Blue";
+         
+         int currentyear=2025;
+         
+         int followers=1000000;
+         
+         float rating=7.5f;
+         
+         char bloodtype='A';
+         
+         String booktitle="To kill a Mockingbird";
+         
+         int numberofemployees =500;
+         
+         String eventtime="2:30PM";
+         
+         String countryname="United states";
+         
+         String eyecolor="Blue";
+         
+         String birthplace ="New York City";
+         
+         float distancebetweencities=200.5f;
+         
+         
+         
 
 	}
 
