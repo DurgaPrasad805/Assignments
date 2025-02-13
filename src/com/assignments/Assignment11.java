9d85bd6356d8d3ea225f587d63823aa73af26c16
Durga Prasad <pdp49507@gmail.com>
2025-02-13 20:38:11 +0530

    Assignment10 - power

---
+++ src/com/assignments/Assignment11.java
@@ -0,0 +1,49 @@
+package com.assignments;
+
+public class Assignment11 {
+
+	public static void main(String[] args) {
+		
+		int base =2;
+		int exponential=10;
+		int result=1;
+		
+		for(int i=0; i<10 ; i++ ) {
+			
+			result =result * 2;
+			
+		}
+		
+		System.out.println("The final output of 2 exponential of 10 is :" + result);
+		
+		double base1 =2.1;
+		int exponential1=3;
+		double result1=1;
+		
+		for(int i=0; i<3 ; i++ ) {
+			
+			result1 =result1 * 2.1;
+			
+		}
+		
+		System.out.println("The final output of 2.1 exponential of 3 is :" + result1);
+		
+		
+		double base2 =2;
+		int exponential2=-2;
+		double result2=-1/2;
+		
+		for(int i=1; i< 2 ; i++ ) {
+			
+			result2 =result2 * 2 ;
+			
+		}
+		
+		System.out.println("The final output of 2 exponential of -2 is :" + result2);
+		
+		
+		
+
+	}
+
+}
