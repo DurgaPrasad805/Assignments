f967deab7694510e9305635174facc1781d48781
DurgaPrasad <pdp49507@gmail.com>
2026-04-19 22:26:36 +0530

    assignment2

--- typescript-training/Assignments/assignment2.ts
+++ typescript-training/Assignments/assignment2.ts
@@ -1,28 +1,29 @@
 function LoanEligibityOfPerson(custonerName:string,creditScore:number,income:number,isEmployed:boolean,debtToIncomeRatio:number):void{
+    
     if(creditScore>=750){
-        console.log("customer eligible for loan"); 
+        console.log(`${custonerName} is eligible for loan`); 
     }else if(creditScore<750 && creditScore>=650){
         if(income==55000.0){
-            console.log("need to verify weather employed or not ")
+            console.log("need to verify you are weather employed or not ");
             if(isEmployed){
-                console.log("Need to check debt to income ratio")
+                console.log(`we need to check debt to income ratio`);
                 if(debtToIncomeRatio<=40.0){
-                    console.log("Eligible for loan")
+                    console.log(`${custonerName} is Eligible for loan`)
                 }else{
-                    console.log("Not eligible due to more debt to income ratio")
+                    console.log(`${custonerName} is Not eligible due to more debt to income ratio`);
                 }
             }else{
-                console.log("not eligine because of unemployed")
+                console.log(`${custonerName} is not eligible because of unemployed`);
             }
         }else{
-            console.log("not eligible for loan because for low income")
+            console.log(` ${custonerName} is not eligible for loan because for low income`);
         }
     }else{
-        console.log("not eligible for loan because low creditscore")
+        console.log(`${custonerName} is not eligible for loan because low creditscore`);
     }
 
 }
- console.log("Prasad is " + LoanEligibityOfPerson("prasad",750,55000.0,true,35.0));
+ LoanEligibityOfPerson("prasad",750,55000.0,true,35.0);
 
 
 
