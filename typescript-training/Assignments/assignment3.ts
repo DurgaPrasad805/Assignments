52867fa0b476026a7c881cf36ccd6cbbae9be093
DurgaPrasad <pdp49507@gmail.com>
2026-04-22 18:46:27 +0530

    assignment3

--- typescript-training/Assignments/assignment3.ts
+++ typescript-training/Assignments/assignment3.ts
@@ -4,18 +4,22 @@ let studentMarks:number[]=[75,80,82];
 let total=0;
 let updatedMarks:number[]=[];
 
-//looping through student marks 
+//looping through student marks to get the each marks
 for(let i=0; i<studentMarks.length;i++){
          let newmarks= studentMarks[i]+10;
          updatedMarks[i]=newmarks;
          total=total+newmarks;
 }
+
+//Average marks of the students
 let average=total/updatedMarks.length;
 
+//Printing the student names with updated marks after addding 10 marks
 console.log("Updated Marks: ");
 for(let i=0; i<studentNames.length;i++){
       console.log(studentNames[i] +" : "+updatedMarks[i]);
 }
 
+//Printing the Average marks of the students
 console.log("Average : " + average );
 
